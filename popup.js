1549cd99230ef502ecb98aebfc93ae885111da5b
tim970303 <tim970303@gmail.com>
2022-12-23 00:33:59 +0800

    fix title bug.

--- popup.js
+++ popup.js
@@ -1,17 +1,18 @@
-chrome.runtime.sendMessage({msg: "getStatus"}, function(response) {
-    console.log(response.lotteryStatus);
-    if (response.lotteryStatus == -1){
-        document.getElementById("title").innerHTML = "未登入。";
-        var login = document.getElementById('login');
-        login.style.display = 'inline-block';
-    }else if(response.lotteryStatus == 1){
-        document.getElementById("title").innerHTML = "今天已經領過了。";
-    }else{
-        document.getElementById("title").innerHTML = "確認中...";
-    }
- });
-
-var button = document.getElementById("loginBtn");
+let intervalID = setInterval(function(){
+    chrome.runtime.sendMessage({msg: "getStatus"}, function(response) {
+        console.log(response.lotteryStatus);
+        if (response.lotteryStatus == -1){
+            document.getElementById("title").innerHTML = "未登入。";
+            var login = document.getElementById('login');
+            login.style.display = 'inline-block';
+            clearInterval(intervalID);
+        }else if(response.lotteryStatus == 1){
+            document.getElementById("title").innerHTML = "今天已經領過了。";
+            clearInterval(intervalID);
+        }
+    });
+},1000)
+let button = document.getElementById("loginBtn");
 button.addEventListener("click", function(){
     chrome.tabs.create({url:"https://login.dlsite.com/login?user=self"});
 });
