0f4044ba8be1b45538e081140b7d06c47298fbbe
tim970303 <tim970303@gmail.com>
2022-12-22 23:42:53 +0800

    UI update.

--- background.js
+++ background.js
@@ -1,13 +1,4 @@
-//not login = -1, init = 0, other = 1,10,100
-let lotteryStatus = 0;
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.msg == "getStatus") {
-        sendResponse({lotteryStatus: lotteryStatus});
-        return true;
-    }
-});
-
-chrome.windows.onCreated.addListener(function() {
+function getPoint(){
     let intervalID = setInterval(function(){
         fetch('https://www.dlsite.com/maniax/event/dlfarm/ajax?act=draw')
         .then((response) => {
@@ -16,13 +7,23 @@ chrome.windows.onCreated.addListener(function() {
         .then((result) => {
             console.log(result['class'][0]);
             if(result['class'][0] == "error"){
-                console.log("今天已經領過了。");
+                lotteryStatus = 1;
                 clearInterval(intervalID);
             }else if(result['class'][1] == "logout"){
-                console.log("未登入。");
                 lotteryStatus = -1;
             }
         })
-    }, 3000)
-})
+    }, 1000)
+}
+//not login = -1, init = 0, other = 1,10,100
+let lotteryStatus = 0;
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.msg == "getStatus") {
+        getPoint();
+        sendResponse({lotteryStatus: lotteryStatus});
+        return true;
+    }
+});
+
+chrome.windows.onCreated.addListener(getPoint())
 
--- popup.js
+++ popup.js
@@ -1,8 +1,13 @@
 chrome.runtime.sendMessage({msg: "getStatus"}, function(response) {
     console.log(response.lotteryStatus);
-    if (response.lotteryStatus != -1){
+    if (response.lotteryStatus == -1){
+        document.getElementById("title").innerHTML = "未登入。";
         var login = document.getElementById('login');
-        login.style.display = "none";
+        login.style.display = 'inline-block';
+    }else if(response.lotteryStatus == 1){
+        document.getElementById("title").innerHTML = "今天已經領過了。";
+    }else{
+        document.getElementById("title").innerHTML = "確認中...";
     }
  });
 
