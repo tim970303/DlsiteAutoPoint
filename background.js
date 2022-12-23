15781d8b6e76a20a4424cfee49722aa1c0a8496b
tim970303 <tim970303@gmail.com>
2022-12-23 18:04:26 +0800

    add "get today's point" functtion.

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+//not login = -1, init = 0, drawed = 1
+let lotteryStatus = 0;
+let point = -1;
+
 function getPoint(){
     let intervalID = setInterval(function(){
         fetch('https://www.dlsite.com/maniax/event/dlfarm/ajax?act=draw')
@@ -6,21 +10,42 @@ function getPoint(){
         })
         .then((result) => {
             console.log(result['class'][0]);
-            if(result['class'][0] == "error"){
+            if((result['class'].length == 1 && result['class'][0] == "error") || (result['class'].length == 2 && result['class'][0].split('_')[0] == "type")){
                 lotteryStatus = 1;
+                caculatePoint();
                 clearInterval(intervalID);
-            }else if(result['class'][1] == "logout"){
+            }else if(result['class'].length == 2 && result['class'][1] == "logout"){
                 lotteryStatus = -1;
             }
         })
     }, 1000)
 }
-//not login = -1, init = 0, other = 1,10,100
-let lotteryStatus = 0;
+
+function caculatePoint(){
+    fetch('https://www.dlsite.com/maniax/event/dlfarm/ajax?act=show')
+    .then((response) => {
+        return response.json();
+    })
+    .then((result) => {
+        let temp = result['class'][2];
+        temp = temp.split("_")[1];
+        temp = parseInt(temp);
+        if(temp == 1){
+            point = 0;
+        }else{
+            temp = (temp - 2) % 3;
+            point = 1;
+            for(let i = 0;i < temp;i++){
+                point = point * 10;
+            }
+        }
+    })
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.msg == "getStatus") {
         getPoint();
-        sendResponse({lotteryStatus: lotteryStatus});
+        sendResponse({lotteryStatus: [lotteryStatus,point]});
         return true;
     }
 });
--- popup.js
+++ popup.js
@@ -1,18 +1,19 @@
 let intervalID = setInterval(function(){
     chrome.runtime.sendMessage({msg: "getStatus"}, function(response) {
         console.log(response.lotteryStatus);
-        if (response.lotteryStatus == -1){
+        if (response.lotteryStatus[0] == -1){
             document.getElementById("title").innerHTML = "未登入。";
             var login = document.getElementById('login');
             login.style.display = 'inline-block';
             clearInterval(intervalID);
-        }else if(response.lotteryStatus == 1){
-            document.getElementById("title").innerHTML = "今天已經領過了。";
+        }else if(response.lotteryStatus[0] == 1){
+            document.getElementById("title").innerHTML = "今天已經抽過了。";
+            document.getElementById("result").innerHTML = ''.concat("今天拿到 ",response.lotteryStatus[1].toString()," 點。");
             clearInterval(intervalID);
         }
     });
 },1000)
 let button = document.getElementById("loginBtn");
 button.addEventListener("click", function(){
-    chrome.tabs.create({url:"https://login.dlsite.com/login?user=self"});
+    chrome.tabs.create({url:"https://www.dlsite.com/maniax/login/=/_query/https%3A%2F%2Fwww.dlsite.com%2Fmaniax%2F"});
 });
